b1141b673e56d226c916c4a81eace25f420e617a
nomayen <nomayen.ohin@gmail.com>
2023-10-09 20:27:41 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,19 @@
+const a = 5;
+const b = 'samsu kioai pel';
+const d = true;
+const age  =[45,65,48];
+const student = {id:23, class:7}
+// console.log(typeof a,typeof b,typeof age,typeof student);
+
+let x = 5;
+let y = x;
+
+console.log(x,y);
+y=7;
+console.log(x,y);
+
+let p ={job:'web Development'}
+let q = p;
+q= {job:'backend'}
+
+console.log(p,q );
